930b9c8b5ac367a2cb77c14222b1276c7c0911d0
tse jerome <tsejerome@tsejero.me>
2020-09-27 18:18:22 +0800

    wip

--- index.js
+++ index.js
@@ -28,6 +28,84 @@ $('body').on('click', '.ok, .close-button', function() {
 
 $('.continue').click(function(){
   $([document.documentElement, document.body]).animate({
-    scrollTop: $(".extra-about-me").offset().top
-}, 2000);
+    scrollTop: $(".tech-stuff-wrapper").offset().top
+  }, 2000);
 })
+
+$('.close-button').on('click',function(){
+  $(this).parents(('.plane')).remove();
+})
+
+$(document).on('scroll', function(){
+  if(window.scrollY + 300 > $('.extra-about-me-main-wrapper .error.box:last-child').position()){
+    const innerTemplate = $('.template.default.chatroom__no-chat__inner-wrapper');
+    const innerDom = innerTemplate.clone();
+    const template = $(".template.chatroom__no-chat__wrapper");
+    const dom = template.clone();
+    innerDom.appendTo($('.extra-about-me-wrapper'));
+    return dom;
+  }
+})
+
+$(function () { 
+  let menuOpened = false;
+  const closeMenu = () =>{
+    $('#menu').removeClass('opened')
+  }
+  const openMenu = () =>{
+    $('#menu').addClass('opened')
+  }
+  const setMenuOpened = () =>{
+    if(menuOpened){ 
+      closeMenu();
+      menuOpened = false;
+    } else{
+      openMenu();
+      menuOpened = true;
+    }
+  }
+  $('.hamburger-trigger').click(function(){
+    setMenuOpened();
+  })
+  
+  $('.main').on('click',function(e){
+    e.preventDefault();
+    e.stopPropagation();
+    if(menuOpened){
+      setMenuOpened();
+    }
+  })
+});
+
+
+
+// (function () {
+//   var body, bodyClick, button, container;
+
+//   button = $('.hamburger-trigger');
+
+//   container = $('.container');
+
+//   body = $('body');
+
+//   bodyClick = function (event) {
+//     if (!$(event.target).closest('.menu').length) {
+//       body.off('click');
+//       return container.toggleClass('menu-open');
+//     }
+//   };
+
+//   button.on('click', function (event) {
+//     event.stopPropagation();
+//     event.preventDefault();
+//     setTimeout(function () {
+//       container.toggleClass('menu-open');
+//     }, 25);
+//     return body.on('click', function (event) {
+//       if (container.hasClass('menu-open')) {
+//         return bodyClick(event);
+//       }
+//     });
+//   });
+
+// }).call(this);
